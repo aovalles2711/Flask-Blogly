603f4b4d8f4dff82fc5227422844ee5c7f27b561
aovalles2711 <124206685+aovalles2711@users.noreply.github.com>
2023-08-29 16:51:59 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 """Models for Blogly."""
-
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -16,11 +16,43 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         """User's full name."""
 
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return fine format for date."""
+
+        return self.created_at.strftime(" %a %b %-d %Y, %-I:%M %p")
+    
+class PostTag(db.Model):
+    """Tagging for posts."""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship('Post', secondary="posts_tag", cascade="all, delete", backref="tags")
 
 def connect_db(app):
     """Connect to database."""
